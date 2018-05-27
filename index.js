e40cbe7e807e0c96ff55dc7fe652dde2056b2e9a
borjagomez <franciscodebo@gmail.com>
2018-05-27 19:39:56 +0200

    Change message.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express()
 
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello Beanstalk!'))
+app.get('/', (req, res) => res.send('Hello Beanstalk dev!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
