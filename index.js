a5f706e48ea20ee9d15dd1cb292da28458606add
borjagomez <franciscodebo@gmail.com>
2018-05-27 19:47:39 +0200

    Fix env variable.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000;
-const environment = process.env.APP_ENV || 'dev';
+const environment = process.env.APP_ENV || 'no-environment';
 
 app.get('/', (req, res) => res.send(`Hello Beanstalk ${environment}!`))
 
